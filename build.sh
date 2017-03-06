d2a0726effd6c6ad9358db6dc79d481ebcfc5030
Jean-Francois Chevrette <jf.cron0@gmail.com>
2017-03-06 14:33:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 GIT_HASH=$(git describe --always --dirty)
 
 CGO_ENABLED=0 GOOS=linux go build github.com/brendandburns/ssl-sidecar
-docker build -t brendanburns/ssl-sidecar:${GIT_HASH} .
-docker push brendanburns/ssl-sidecar:${GIT_HASH}
+docker build -t jfchevrette/ssl-sidecar:${GIT_HASH} .
+docker push jfchevrette/ssl-sidecar:${GIT_HASH}
 
 rm ssl-sidecar
